30d74e0b860f4030974a5fb5d87fb78585de416d
root <root@DESKTOP-T7HLBQL.localdomain>
2023-02-05 17:11:04 +0100

    Detect inputs differents than char

--- client/src/components/CodeSpace.vue
+++ client/src/components/CodeSpace.vue
@@ -5,9 +5,24 @@
 
     const string = ref("Input : ");
 
-    // Detect user inputs and add it to the string
+    // Handle char inputs
     window.addEventListener("keypress", function(e) {
+
         string.value = string.value + String.fromCharCode(e.keyCode);
+        
+    }.bind(this));
+
+    // Handle inputs as Escape, Backspace etc...
+    window.addEventListener("keydown", function(e) {
+
+        switch (e.key) {
+
+            // Delete last char
+            case "Backspace":
+                string.value = string.value.slice(0, -1);
+                break;
+        }
+        console.log(e);
     }.bind(this));
 
     // String that take use input
