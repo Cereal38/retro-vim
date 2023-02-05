d1f34c75a534eae650a3c55503e54a5258c9a22d
root <root@DESKTOP-T7HLBQL.localdomain>
2023-02-05 15:46:05 +0100

    Layout inited

--- client/src/App.vue
+++ client/src/App.vue
@@ -4,7 +4,7 @@ import LayoutTerminal from "@/layouts/LayoutTerminal.vue"
 </script>
 
 <template>
-  <div>
+  <!-- <div>
     <a href="https://vitejs.dev" target="_blank">
       <img src="/vite.svg" class="logo" alt="Vite logo" />
     </a>
@@ -12,11 +12,13 @@ import LayoutTerminal from "@/layouts/LayoutTerminal.vue"
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <HelloWorld msg="Vite + Vue" /> -->
+  <LayoutTerminal />
 </template>
 
 <style scoped>
-.logo {
+
+/* .logo {
   height: 6em;
   padding: 1.5em;
   will-change: filter;
@@ -27,5 +29,5 @@ import LayoutTerminal from "@/layouts/LayoutTerminal.vue"
 }
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883ff);
-}
+} */
 </style>
--- client/src/layouts/LayoutTerminal.vue
+++ client/src/layouts/LayoutTerminal.vue
@@ -1,2 +1,32 @@
 <script setup lang="ts">
 </script>
+
+
+<template>
+    <div class="layout-terminal__main">
+
+        <h1 class="temp-title">Hello world; It start now !</h1>
+
+    </div>
+</template>
+
+
+<style scoped>
+
+@import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
+
+.layout-terminal__main {
+    display: flex;
+    flex-direction: column;
+    justify-content: flex-start;
+    align-items: flex-start;
+    height: 100vh;
+    width: 100vw;
+    background-color: #0A000A;
+    font-family: "Press Start 2P", cursive;
+}
+
+.temp-title {
+    color: green;
+}
+</style>
