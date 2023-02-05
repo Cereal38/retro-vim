17b215d19305a19c903e2c461c71cebaa9dac509
root <root@DESKTOP-T7HLBQL.localdomain>
2023-02-05 17:32:04 +0100

    Allow to display multilines

--- client/src/components/CodeSpace.vue
+++ client/src/components/CodeSpace.vue
@@ -3,13 +3,13 @@
     import { ref } from "vue";
 
 
-    const string = ref("Input : ");
+    const string = ref(["1 : ", "2 : "]);
 
     // Handle char inputs
     window.addEventListener("keypress", function(e) {
 
-        string.value = string.value + String.fromCharCode(e.keyCode);
-        
+        string.value[0] = string.value[0] + String.fromCharCode(e.keyCode);
+
     }.bind(this));
 
     // Handle inputs as Escape, Backspace etc...
@@ -19,7 +19,7 @@
 
             // Delete last char
             case "Backspace":
-                string.value = string.value.slice(0, -1);
+                string.value[0] = string.value[0].slice(0, -1);
                 break;
         }
         console.log(e);
@@ -32,9 +32,17 @@
 
 
 <template>
-    <div class="code-space__main">
+    <div class="code-space">
 
-        <span>{{ string }}</span>
+        <ul class="code-space__lines">
+
+            <li 
+                class="code-space__lines__line" 
+                v-for="line in string" 
+                :key="line">{{ line }}
+            </li>
+
+        </ul>
 
     </div>
 </template>
@@ -42,12 +50,19 @@
 
 <style scoped>
 
-    .code-space__main {
-        display: flex;
+    .code-space {
         height: 100%;
         width: 100%;
+    }
+
+    .code-space__lines {
+        display: flex;
         flex-direction: column;
         gap: 20px;
+    }
+
+    .code-space__lines__line {
+        all: unset;
     }
 
 </style>
--- client/src/layouts/LayoutTerminal.vue
+++ client/src/layouts/LayoutTerminal.vue
@@ -4,7 +4,7 @@ import CodeSpace from "@/components/CodeSpace.vue"
 
 
 <template>
-    <div class="layout-terminal__main">
+    <div class="layout-terminal">
 
         <CodeSpace />
 
@@ -16,7 +16,7 @@ import CodeSpace from "@/components/CodeSpace.vue"
 
 @import url('https://fonts.googleapis.com/css2?family=Press+Start+2P&display=swap');
 
-.layout-terminal__main {
+.layout-terminal {
     display: flex;
     padding: 20px;
     height: calc(100vh - 40px);
@@ -26,9 +26,4 @@ import CodeSpace from "@/components/CodeSpace.vue"
     color: #55AA00;
 }
 
-.layout-terminal__main__code-space {
-    display: flex;
-    flex-direction: column;
-    gap: 20px;
-}
 </style>
