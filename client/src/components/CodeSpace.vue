4a9a2139fba67f463a49dc9feb9bde3c408e7c28
root <root@DESKTOP-T7HLBQL.localdomain>
2023-02-05 22:25:30 +0100

    Backspace is working

--- client/src/components/CodeSpace.vue
+++ client/src/components/CodeSpace.vue
@@ -2,14 +2,21 @@
 
     import { ref } from "vue";
 
+    const initialRowLen = 5;
+
     // Current cursor position (x : horizontal / y : vertical)
     const cursor = ref({
-        x: 0,
+        x: initialRowLen,
         y: 0,
     })
 
+    const checkCursorPosition = (x: number, y: number) => {
+        if (x < initialRowLen || x > 85 || y < 0 || y > 1000) { return 0 }
+        return 1;
+    }
+
     // Lines of code
-    const lines = ref([""]);
+    const lines = ref(["\r"]);
 
     // Handle char inputs
     window.addEventListener("keypress", function(e) {
@@ -33,35 +40,38 @@
             // Delete last char
             // If the line is empty, delete the line (Except for the first one)
             case "Backspace":
-                if (lines.value[cursor.value.y].length === 1 && lines.value.length > 1) {
+
+                if (cursor.value.y > 0 && lines.value[cursor.value.y].length === 1) {
                     lines.value.splice(cursor.value.y, 1);
                     cursor.value.y--;
-                    cursor.value.x = lines.value[cursor.value.y].length-1;
+                    cursor.value.x = lines.value[cursor.value.y].length - 1 + initialRowLen;
                 } else {
+                    if (!checkCursorPosition(cursor.value.x - 1, cursor.value.y)) { return }
                     lines.value[cursor.value.y] = lines.value[cursor.value.y].slice(0, -1);
                     cursor.value.x--;
                 }
+
                 break;
             
             // Create a new line and put cursor on it
             case "Enter":
                 lines.value.push("");
                 cursor.value.y++;
-                cursor.value.x = 0;
+                cursor.value.x = initialRowLen;
                 break;
             
             // Add a tab
             case "Tab":
                 event?.preventDefault(); // Remove the normal behavior of the tab key
                 lines.value[cursor.value.y] = lines.value[cursor.value.y] + '    ';
+                cursor.value.x += 4;
                 break;
 
             default:
                 break;
         }
-        
-        // DEBUG
-        console.log(lines.value)
+
+        console.log(lines)
 
     }.bind(this));
 
@@ -82,10 +92,33 @@
                 gridRow: indexLine + 1,
             }"
         >
+
+            <span
+                :style="{
+                    gridColumn: 1,
+                    gridRow: indexLine + 1,
+                }"
+            >
+                {{ indexLine + 1 }}
+            </span>
+
+            <span
+                :style="{
+                    gridColumn: 4,
+                    gridRow: indexLine + 1,
+                }"
+            >
+                :
+            </span>
+
             <span 
                 class="code-space__lines__line__content" 
                 v-for="(char, indexChar) in line"
                 :key="indexChar"
+                :style="{
+                    gridColumn: initialRowLen + indexChar,
+                    gridRow: indexLine + 1,
+                }"
             >
                 <span 
                     v-html="char.replace(/ /g, '&nbsp;')"
@@ -95,7 +128,7 @@
 
         <!-- Cursor -->
         <div 
-            class="code-space__cursor"
+            class="code-space__cursor-insert"
             :style="{
                 gridColumn: cursor.x + 1,
                 gridRow: cursor.y + 1,
@@ -118,8 +151,14 @@
         font-size: 20px;
     }
 
-    .code-space__cursor {
-        height: 20px;
+    .code-space__lines__line {
+        display: grid;
+        grid-template-columns: repeat(80, 20px);
+    }
+
+    .code-space__cursor-insert {
+        margin-top: 15px;
+        height: 5px;
         width: 20px;
         background-color: aliceblue;
     }
