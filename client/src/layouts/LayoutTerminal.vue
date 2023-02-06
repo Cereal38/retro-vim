0f9a17c51f83f2b8fb691df8f11ebb737f534535
Cereal <guillaume.bruncosmegazot@gmail.com>
2023-02-06 17:18:33 +0100

    Background style done

--- client/src/layouts/LayoutTerminal.vue
+++ client/src/layouts/LayoutTerminal.vue
@@ -1,14 +1,16 @@
 <script setup lang="ts">
-import CodeSpace from "@/components/CodeSpace.vue"
+	import CodeSpace from "@/components/CodeSpace.vue"
 </script>
 
 
 <template>
+
+	<div class="layout-terminal__layer" />
+
     <div class="layout-terminal">
-
-        <CodeSpace />
-
+			<CodeSpace />
     </div>
+
 </template>
 
 
@@ -23,7 +25,17 @@ import CodeSpace from "@/components/CodeSpace.vue"
     width: calc(100vw - 40px);
     font-family: "Press Start 2P", cursive;
     background-color: #1d1d1d;
-    color: #12da33dc;
+    color: #52fa33fc;
+}
+
+.layout-terminal__layer {
+	position: fixed;
+	width: 100vw;
+	height: 100vh;
+	filter: brightness(0.7) contrast(0.8);
+	background-color: #00a500;
+	opacity: 0.5;
+	box-shadow:inset 0px 0px 50vh 150px #002500;
 }
 
 </style>
